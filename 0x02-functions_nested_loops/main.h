c5b07b7c8c72957ab27165324fff22576dd5cdc3
justonlyme1 <taiwomiracleveecthor@gmail.com>
2023-08-20 17:07:21 -0700

    added main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+void print_alphabet(void);
+void jack_bauer(void);
+void print_alphabet_x10(void);
+void times_table(void);
+void print_to_98(int n);
+void print_times_table(int n);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int add(int n, int m);
+int print_last_digit(int n);
+
+#endif /* MAIN_H */

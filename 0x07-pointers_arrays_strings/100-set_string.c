43cecc6999f07dfb3172225c621416e9a9662f74
justonlyme1 <taiwomiracleveecthor@gmail.com>
2023-08-30 04:38:11 -0700

    The set_string function takes a pointer to a pointer to a char s and a pointer to a char to.

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * set_string - Sets the value of a pointer to a char.
- * @s: Pointer to a pointer to a char.
- * @to: Pointer to a char to set the value to.
+ * set_string - sets the value of a pointer to a char
+ * @s: source adress.
+ * @to: target adress.
+ * Return: no return.
  */
 void set_string(char **s, char *to)
 {
-    *s = to; // Set the value of the pointer to 'to'
+	*s = to;
 }
